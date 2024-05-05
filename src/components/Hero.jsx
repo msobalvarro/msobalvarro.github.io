f8624186ff3b4f4339d526b491b2addde8cb45f3
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-05-04 22:04:42 -0600

    configure icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,27 +7,26 @@ const Hero = () => {
     <div className='max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center text-white'>
       <img className='object-scale-down h-48 w-48 self-center girar-animacion' src={reactImage} />
 
-      <h1 className='md:text-5xl sm:text-6xl text-4xl font-bold md:py-6 p-0'>
+      <h1 className='md:text-5xl sm:text-6xl text-4xl font-bold p-0'>
         SAMUEL SOBALVARRO
       </h1>
-
-      <p className='text-[#61dbfb] font-bold'>
-        DESARROLLADOR JAVASCRIPT FULLSTACK
-      </p>
 
       <div className='flex justify-center items-center'>
         <p className='md:text-4xl sm:text-3xl text-xl font-bold py-4'>
           Desarrollo de aplicaciones con
+          <ReactTyped
+            className='md:text-4xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 text-[#61dbfb]'
+            strings={['React.js', 'React-Native', 'Node.js', 'Express']}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </p>
-
-        <ReactTyped
-          className='md:text-4xl sm:text-4xl text-xl font-bold md:pl-4 pl-2 text-[#61dbfb]'
-          strings={['React.js', 'React-Native', 'Node.js', 'Express']}
-          typeSpeed={120}
-          backSpeed={140}
-          loop
-        />
       </div>
+
+      <p className='text-[#61dbfb] font-bold'>
+        DESARROLLADOR JAVASCRIPT FULLSTACK
+      </p>
 
       <button className='bg-[#61dbfb] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>
         <a href='https://drive.google.com/file/d/1fb3GrIGErPOeugANgOwDWIquApqFkwll/view?usp=drive_link' target='_blank'>
